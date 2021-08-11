bc367ed7aba80a1f78504731df097cb7b8d9bf08
harsh <h@g.com>
2021-08-12 00:07:58 +0530

    added board state and also react events

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,23 +1,52 @@
-import React from 'react';
-import Sqaure from './Sqaure';
+import React, { useState } from 'react';
+import Sqaure from './Square';
 
 const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const handleSquareClick = position => {
+    if (board[position]) {
+      return;
+    }
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square;
+      });
+    });
+
+    setIsXNext(prev => !prev);
+  };
+
+  const renderSquare = position => {
+    return (
+      <Sqaure
+        value={board[position]}
+        onClick={() => handleSquareClick(position)}
+      />
+    );
+  };
+
   return (
     <div className="board">
       <div className="board-row">
-        <Sqaure value={0} />
-        <Sqaure value={1} />
-        <Sqaure value={2} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Sqaure value={3} />
-        <Sqaure value={4} />
-        <Sqaure value={5} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Sqaure value={6} />
-        <Sqaure value={7} />
-        <Sqaure value={8} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
---
+++ src/components/Square.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sqaure = ({ value, onClick }) => {
+  return (
+    <button type="button" className="square" onClick={onClick}>
+      {value}
+    </button>
+  );
+};
+
+export default Sqaure;
